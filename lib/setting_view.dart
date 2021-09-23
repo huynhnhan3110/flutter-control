9e438fb383a2a4a97fe2957ec221e129472f8c63
Nhan <huynhnhan.dev@gmail.com>
2021-09-23 10:11:38 +0700

    tach file

--- lib/setting_view.dart
+++ lib/setting_view.dart
@@ -16,7 +16,6 @@ class _SettingViewState extends State<SettingView> {
   String _ipString;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getIPValuesSF();
   }
